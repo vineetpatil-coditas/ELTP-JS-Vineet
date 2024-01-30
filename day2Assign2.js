2b8de0dc17343d1136f8bf19d75b8965aacbb1eb
vineetpatil-coditas <158129324+vineetpatil-coditas@users.noreply.github.com>
2024-01-30 16:31:51 +0530

    Add files via upload

---
+++ day2Assign2.js
@@ -0,0 +1,30 @@
+//day2 assignment
+// Declare four variables without assigning values
+// Declare four variables with assigned values
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+let name;
+let contactNumber;
+let userName;   
+let password;
+// Declaring four variables with assigned values
+const internFirstName ="Jaykumar";
+const internLastName="Pokar";
+const internMaritalStatus="Married";
+const internNationality="India";
+const internAge=22;
+// Declaring variables to store your first name, last name, marital status, country and age in a single line
+let firstName="Vineet",lastName="Patil",maritalStatus="Single",age=22;
+// Declaring two variables myAge and yourAge and assign them initial values and log to the browser console
+let myAge=21;
+let yourAge=22;
+console.log("myAge"+myAge)
+console.log("yourAge"+yourAge)
+
+
+
+
+
+
+
